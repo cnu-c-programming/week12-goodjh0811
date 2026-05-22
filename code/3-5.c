4ed1a7d908f5c0dd9eac3f7eff03d9942c2b390f
박지현 <goodjh0811@gmail.com>
2026-05-22 10:36:24 +0900

    week12

--- code/3-5.c
+++ code/3-5.c
@@ -7,10 +7,17 @@ int main(int argc, const char* argv[]) {
     if (fp == NULL) {
         return 0;
     }
-
-
+    char line[100];
     int sum = 0;
+    char extra;
 
+    while (fgets(line, sizeof(line), fp) != NULL) {
+        if (sscanf(line, "%d %c", &sum, &extra) == 1) {
+            sum += sum;
+        } else {
+            fprintf(stderr, "invalid input: %s", line);
+        }
+    }
 
     printf("sum: %d\n", sum);
     fclose(fp);
